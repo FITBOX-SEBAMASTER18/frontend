1efe08887e8c02c44e1d2caad8579b1db564d47e
Haydar Sahin <haydar.sahin@tum.de>
2018-06-26 19:18:45 +0200

    bugfix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { MainView } from './views/MainView';
 import { LoginView } from './views/LoginView';
-import { SignupView } from './views/SignupView';
 import { MenuView } from './views/MenuView';
 import { RegisterView } from './views/RegisterView';
 
@@ -16,9 +15,8 @@ export default class App extends React.Component {
         routes: [
             { component: MainView , path: '/', exact: true},
             { component: LoginView, path: '/login'},
-            { component: SignupView, path: '/register'},
+            { component: RegisterView, path: '/register'},
             { component: MenuView, path: '/menu'},
-            { component: RegisterView, path: '/register'}
         ]
     };
     document.title = this.state.title;
