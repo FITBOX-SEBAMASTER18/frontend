385bede1e70f9921958aa11ba709cf4f3d768f2e
kuzdogan <kaanuzdogan@gmail.com>
2018-06-28 18:27:06 +0200

    file-loader for loading images

--- src/components/Home/FirstComponent.js
+++ src/components/Home/FirstComponent.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Grid, Cell, Media, CardTitle, MediaOverlay} from 'react-md';
+import image1 from './home-meal.jpg';
 
 export default () => {
     return (
 	    	<Media aspectRatio="16-9">
-	    		<img src="https://dynamicmedia.zuza.com/zz/m/original_/4/5/45d5ab38-5b77-45d6-b685-21b90f8c9f79/Column-4-Food___Super_Portrait.jpg"/>
+	    		<img src={image1}/>
 	    			<MediaOverlay>
 						<CardTitle title="Wow. Amazing." />
 					</MediaOverlay>
