cf136d63e9f5c38d42ae716d059a1708b83abba6
kuzdogan <kaanuzdogan@gmail.com>
2018-06-30 18:40:00 +0200

    Logout button and notification

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import UserService from "../services/UserService";
-import { Toolbar, Button, Tabs, Tab, TabsContainer } from 'react-md';
+import { Toolbar, Button, Tabs, Tab, TabsContainer, Snackbar } from 'react-md';
 import { withRouter } from 'react-router-dom'
 import HeaderTabs from './HeaderTabs'
 import logo from '../../public/FitBox.png'
@@ -13,10 +13,34 @@ class Header extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = { toasts: [], autohide: true };          
+        this.handleLogout = this.handleLogout.bind(this);
+        this.addToast = this.addToast.bind(this);
+        this.dismissToast = this.dismissToast.bind(this);
     }
+ 
 
+    addToast(text, action, autohide = true){
+        this.setState((state) => {
+          const toasts = state.toasts.slice();
+          toasts.push({ text, action });
+          return { toasts, autohide };
+        });
+    };
+
+    handleLogout(){
+        UserService.logout();
+        this.props.history.push("/");
+        this.addToast('Logged Out!');
+    }
+    dismissToast(){
+        const [, ...toasts] = this.state.toasts;
+        this.setState({ toasts });
+    };
     render() {
+        const { toasts, autohide } = this.state;
         return (
+            <div>
             <Toolbar
                 colored
                 nav={<div className="header-logo">
@@ -29,11 +53,23 @@ class Header extends React.Component {
                 }
                 actions={
                     (UserService.isAuthenticated())
-                    ? <Button className="shopping-cart" floating secondary onClick={() => this.props.history.push('/cart')}>shopping_cart</Button>
+                    ?     
+                        <div className="header-right-buttons">
+                            <Button className="shopping-cart" tooltipLabel="Shopping Cart" floating secondary onClick={() => this.props.history.push('/cart')}>shopping_cart</Button>
+                            <Button className="logout" tooltipLabel="Logout" floating secondary onClick={this.handleLogout}>power_settings_new</Button>
+                        </div>
+                    
                     : <Button className="header-login" raised secondary onClick={() => this.props.history.push('/login')} >Login</Button>                
                 }
             >
             </Toolbar>
+            <Snackbar
+              id="example-snackbar"
+              toasts={toasts}
+              autohide={autohide}
+              onDismiss={this.dismissToast}
+            />
+            </div>
         );
     }
 };
