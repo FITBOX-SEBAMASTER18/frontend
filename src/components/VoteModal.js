89bd1cda2ed4b6b222ff0c12d761c8ccd3f04821
kuzdogan <kaanuzdogan@gmail.com>
2018-07-01 18:02:34 +0200

    Voting modal ui

--- src/components/VoteModal.js
+++ src/components/VoteModal.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 
 import { List, ListItem, TextField, Button, DialogContainer, Grid, Cell, Media, MediaOverlay, CardTitle } from 'react-md';
-
+import './vote-modal.scss';
 
 class VoteModal extends React.Component {
     constructor (props) {
@@ -25,38 +25,53 @@ class VoteModal extends React.Component {
         console.log("didnt return")
         return (  
             <DialogContainer
-              id="simple-list-dialog"
+              id="voting-dialog"
               visible={visible}
-              title="Simple List Dialog"
+              title="Vote Next Week's Meals"
               onHide={this.hide}
               focusOnMount={false}
-              height="50%"
+              height="70%"
               width="70%"
             >
-            <Grid>
-                <Cell size = {12} className="vote-title">
-                <p> Vote Next Week's Meals </p>
-            </Cell>
+                <Grid className="vote-meal-grid">
                     <Cell size = {4}>
                         <Media><img src={this.props.meals[0].image} />
 
                         </Media>
-                        <p> {this.props.meals[0].name} </p>
-                        <Button raised primary iconClassName="fa fa-hand-spock-o">Spock</Button>
+                        <p className="meal-name"> {this.props.meals[0].name} </p>
+                        <p> Calories: {this.props.meals[0].calories} </p>
+                        <p> Fat: {this.props.meals[0].calories} </p>
+                        <p> Carbs: {this.props.meals[0].carbohydrates} </p>
+                        <p> Proteins: {this.props.meals[0].protein} </p>
+                        <Grid>
+                            <Button className="vote-button"raised primary iconClassName="fa fa-thumbs-up">Vote</Button>
+                        </Grid>
                     </Cell>
                     <Cell size = {4}>
                         <Media><img src={this.props.meals[1].image} />
 
                         </Media>
-                        <p> {this.props.meals[1].name} </p>
-                        <Button> Vote </Button>
+                        <p className="meal-name"> {this.props.meals[1].name} </p>
+                        <p> Calories: {this.props.meals[1].calories} </p>
+                        <p> Fat: {this.props.meals[1].calories} </p>
+                        <p> Carbs: {this.props.meals[1].carbohydrates} </p>
+                        <p> Proteins: {this.props.meals[1].protein} </p>
+                        <Grid>
+                            <Button className="vote-button" raised primary iconClassName="fa fa-thumbs-up">Vote</Button>
+                        </Grid>
                     </Cell>
                     <Cell size = {4}>
                         <Media><img src={this.props.meals[2].image} />
 
                         </Media>
-                        <p> {this.props.meals[2].name} </p>
-                        <Button> Vote </Button>
+                        <p className="meal-name"> {this.props.meals[2].name} </p>
+                        <p> Calories: {this.props.meals[2].calories} </p>
+                        <p> Fat: {this.props.meals[2].calories} </p>
+                        <p> Carbs: {this.props.meals[2].carbohydrates} </p>
+                        <p> Proteins: {this.props.meals[2].protein} </p>
+                        <Grid>
+                            <Button className="vote-button" raised primary iconClassName="fa fa-thumbs-up">Vote</Button>
+                        </Grid>
                     </Cell>
                 </Grid>
             </DialogContainer>
