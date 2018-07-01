8f6c0da3cd8a21f753cd12a5770ce4af0618de5e
Haydar Sahin <haydar.sahin@tum.de>
2018-07-01 20:32:17 +0200

    fetch orders from server

---
+++ src/services/OrderService.js
@@ -0,0 +1,22 @@
+"use strict";
+
+import HttpService from './HttpService';
+
+export default class OrderService {
+
+    constructor(){
+    }
+
+    static baseURL() {return "http://localhost:6678/order" }
+
+    static getOrdersOfUser() {
+        let url = OrderService.baseURL() + "/user/"
+        return new Promise((resolve, reject) => {
+           HttpService.get(url, function(data) {
+               resolve(data);
+           }, function(textStatus) {
+               reject(textStatus);
+           });
+        });
+    }
+}
